f68339d157ac524ae57cff1c90f7dcc308bb1781
hijkzzz <janhu9527@gmail.com>
2018-06-08 18:42:54 +0800

    fix

--- ddpg.py
+++ ddpg.py
@@ -15,8 +15,8 @@ REPLAY_SIZE = 10000
 NUM_EPISODES = 100000
 GAMMA = 0.9
 TAU = 0.1
-EXPLORATION_END = 1000
-UPDATES_PER_STEP = 2
+EXPLORATION_END = 100
+UPDATES_PER_STEP = 1
 
 torch.manual_seed(SEED)
 np.random.seed(SEED)
@@ -25,7 +25,7 @@ np.random.seed(SEED)
 def main():
     net = DDPG(GAMMA, TAU, True)
     memory = ReplayMemory(REPLAY_SIZE)
-    ounoise = OUNoise(1, scale=1)
+    ounoise = OUNoise(1, scale=0.8)
 
     state = env.get_init_state()
     updates = 0
@@ -58,9 +58,10 @@ def main():
                 print(
                     "Episode: {}, Updates: {}, Value Loss: {}, Policy Loss: {}".
                     format(i_episode, updates, value_loss, policy_loss))
+                break
 
-            if (i_episode + 1) % 1000 == 0:
-                net.save_model()
+        if (i_episode + 1) % 10000 == 0:
+            net.save_model()
 
 if __name__ == "__main__":
     main()
