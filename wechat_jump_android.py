307b5c6f20a02a81f2252b510f95c384c9e6a174
hijkzzz <janhu9527@gmail.com>
2018-06-09 00:08:08 +0800

    fix

--- wechat_jump_android.py
+++ wechat_jump_android.py
@@ -141,7 +141,7 @@ def step(action):
         reward = score - last_score
         reward = reward if reward > 0 else -1
         last_score = score
-        mask = 1
+        mask = 1 if reward > 0 else 0 # 原地跳动当作游戏结束惩罚
 
     print("Press Time: {} ms, Mask: {}, Reward: {}".format(press_time, mask, reward))
 
